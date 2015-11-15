d127467f7f4f83fbfd0d51b7f7e9210bdbbac1dc
czqInNanjing <1300236145@qq.com>
2015-11-15 09:43:00 +0800

    123

--- ELMSClient/src/bl/userbl/UserMes.java
+++ ELMSClient/src/bl/userbl/UserMes.java
@@ -6,39 +6,38 @@ import ds.accountdataservice.AccountDataService;
 import util.ResultMessage;
 import vo.AccountVO;
 import blservice.usermesblservice.UserMesblservice;
+
 /**
  * 
  * @author czq
  *
  */
-<<<<<<< HEAD:ELMSClient/src/bl/userbl/UserMes.java
+
 public class UserMes {
-	AccountDataService accountds ;
-=======
-public class UserMesBusinessServiceImpl implements UserMesblservice {
 	AccountDataService accountds;
->>>>>>> origin/master:ELMSClient/src/bl/userbl/UserMesBusinessServiceImpl.java
-	
-	
-	
-	public ResultMessage login(AccountVO vo)  {
-		try {
-			return accountds.check(vo.ID, vo.password);
-			
-		} catch (RemoteException e) {
-			e.printStackTrace();
-			return ResultMessage.FAIL;
+
+	public class UserMesBusinessServiceImpl implements UserMesblservice {
+		AccountDataService accountds;
+
+		public ResultMessage login(AccountVO vo) {
+			try {
+				return accountds.check(vo.ID, vo.password);
+
+			} catch (RemoteException e) {
+				e.printStackTrace();
+				return ResultMessage.FAIL;
+			}
 		}
-	}
 
-	public ResultMessage modify(AccountVO vo) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		public ResultMessage modify(AccountVO vo) {
+			// TODO Auto-generated method stub
+			return null;
+		}
 
-	public AccountVO getMes() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		public AccountVO getMes() {
+			// TODO Auto-generated method stub
+			return null;
+		}
 
+	}
 }
