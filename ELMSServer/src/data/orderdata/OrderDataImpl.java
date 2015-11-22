92f30742ce7c18993e0b63a3047f8dba04f700de
czqInNanjing <1300236145@qq.com>
2015-11-22 14:34:15 +0800

    来多一次

--- ELMSServer/src/data/orderdata/OrderDataImpl.java
+++ ELMSServer/src/data/orderdata/OrderDataImpl.java
@@ -44,9 +44,29 @@ public class OrderDataImpl extends DataSuperClass implements OrderDataService {
 		try {
 			preState = conn.prepareStatement(sql);
 			result = preState.executeQuery();
-			while(result.next()){
-				po = new OrderPO(result.getString(1),DocType.valueOf(result.getString(2)) , MyDate.getDate(result.getString(3) ),
-						DocState.valueOf(result.getString(4)), new PeopleMes(result.getString(5), result.getString(6), result.getString(7), result.getString(8)) , new PeopleMes(result.getString(9), result.getString(10), result.getString(11), result.getString(12)) , new GoodMes(Integer.parseInt(result.getString(13)), result.getString(14), Integer.parseInt(result.getString(15)), Integer.parseInt(result.getString(16)), Integer.parseInt(result.getString(17)), Integer.parseInt(result.getString(18))) , new OtherOrderMes(result.getString(19), result.getString(20), Integer.parseInt(result.getString(21)), Integer.parseInt(result.getString(22)), result.getString(28), MyDate.getDate(result.getString(29)) ), new TransferDocs(result.getString(23), result.getString(23), result.getString(23), result.getString(23), result.getString(23)));
+			while (result.next()) {
+				po = new OrderPO(result.getString(1), DocType.valueOf(result
+						.getString(2)), MyDate.getDate(result.getString(3)),
+						DocState.valueOf(result.getString(4)), new PeopleMes(
+								result.getString(5), result.getString(6),
+								result.getString(7), result.getString(8)),
+						new PeopleMes(result.getString(9),
+								result.getString(10), result.getString(11),
+								result.getString(12)), new GoodMes(
+								Integer.parseInt(result.getString(13)),
+								result.getString(14), Integer.parseInt(result
+										.getString(15)),
+								Integer.parseInt(result.getString(16)),
+								Integer.parseInt(result.getString(17)),
+								Integer.parseInt(result.getString(18))),
+						new OtherOrderMes(result.getString(19), result
+								.getString(20), Integer.parseInt(result
+								.getString(21)), Integer.parseInt(result
+								.getString(22)), result.getString(28), MyDate
+								.getDate(result.getString(29))),
+						new TransferDocs(result.getString(23), result
+								.getString(24), result.getString(25), result
+								.getString(26), result.getString(27)));
 				pos.add(po);
 			}
 			
@@ -119,24 +139,28 @@ public class OrderDataImpl extends DataSuperClass implements OrderDataService {
 		if(findMes == null){
 			return null;
 		}else{
-			return new OrderPO(findMes.get(1), MyDate.getDate(findMes
-					.get(2)), findMes.get(3),
-					findMes.get(4), findMes.get(5),
-					findMes.get(6), findMes.get(7),
-					findMes.get(8), findMes.get(9),
-					findMes.get(10), Integer.parseInt(findMes.get(11)),
-					findMes.get(12),
-					Integer.parseInt(findMes.get(13)),
-					Integer.parseInt(findMes.get(14)),
-					Integer.parseInt(findMes.get(15)),
-					Integer.parseInt(findMes.get(16)),
-					findMes.get(17), findMes.get(18),
-					Integer.parseInt(findMes.get(19)),
-					Integer.parseInt(findMes.get(20)),
-					findMes.get(21), findMes.get(22),
-					findMes.get(23), findMes.get(24),
-					findMes.get(25), findMes.get(26),
-					MyDate.getDate(findMes.get(27)));
+			return  new OrderPO(findMes.get(0), DocType.valueOf(findMes
+					.get(0)), MyDate.getDate(findMes.get(2)),
+					DocState.valueOf(findMes.get(3)), new PeopleMes(
+							findMes.get(4), findMes.get(5),
+							findMes.get(6), findMes.get(7)),
+					new PeopleMes(findMes.get(8),
+							findMes.get(9), findMes.get(10),
+							findMes.get(11)), new GoodMes(
+							Integer.parseInt(findMes.get(12)),
+							findMes.get(13), Integer.parseInt(findMes
+									.get(14)),
+							Integer.parseInt(findMes.get(15)),
+							Integer.parseInt(findMes.get(16)),
+							Integer.parseInt(findMes.get(17))),
+					new OtherOrderMes(findMes.get(18), findMes
+							.get(19), Integer.parseInt(findMes
+							.get(20)), Integer.parseInt(findMes
+							.get(21)), findMes.get(27), MyDate
+							.getDate(findMes.get(28))),
+					new TransferDocs(findMes.get(22), findMes
+							.get(23), findMes.get(24), findMes
+							.get(25), findMes.get(26)));
 		}
 		
 	}
@@ -182,8 +206,45 @@ public class OrderDataImpl extends DataSuperClass implements OrderDataService {
 	}
 
 	public ArrayList<? extends DocPO> getDocLists(DocType type) {
-		// TODO Auto-generated method stub
-		return null;
+		sql = "SELECT * FROM " + orderTable + "WHERE state = wait" ;
+		ArrayList<OrderPO> pos = new ArrayList<OrderPO>(50);
+		OrderPO po;
+		try {
+			preState = conn.prepareStatement(sql);
+			result = preState.executeQuery();
+			while (result.next()) {
+				po = new OrderPO(result.getString(1), DocType.valueOf(result
+						.getString(2)), MyDate.getDate(result.getString(3)),
+						DocState.valueOf(result.getString(4)), new PeopleMes(
+								result.getString(5), result.getString(6),
+								result.getString(7), result.getString(8)),
+						new PeopleMes(result.getString(9),
+								result.getString(10), result.getString(11),
+								result.getString(12)), new GoodMes(
+								Integer.parseInt(result.getString(13)),
+								result.getString(14), Integer.parseInt(result
+										.getString(15)),
+								Integer.parseInt(result.getString(16)),
+								Integer.parseInt(result.getString(17)),
+								Integer.parseInt(result.getString(18))),
+						new OtherOrderMes(result.getString(19), result
+								.getString(20), Integer.parseInt(result
+								.getString(21)), Integer.parseInt(result
+								.getString(22)), result.getString(28), MyDate
+								.getDate(result.getString(29))),
+						new TransferDocs(result.getString(23), result
+								.getString(24), result.getString(25), result
+								.getString(26), result.getString(27)));
+				pos.add(po);
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return pos;
 	}
 
 	public ResultMessage changeDocsState(ArrayList<String> docsID,
