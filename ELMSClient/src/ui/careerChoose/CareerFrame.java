733ef1bb8c8341e41b67cf75b2a8b2f1c4dfdec8
junooo <384933326@qq.com>
2015-11-26 08:42:41 +0800

    1

--- ELMSClient/src/ui/careerChoose/CareerFrame.java
+++ ELMSClient/src/ui/careerChoose/CareerFrame.java
@@ -1,9 +1,27 @@
 package ui.careerChoose;
+
+import org.dom4j.Element;
+
+import ui.tools.MyFrame;
+
 /**
 *
 *@author:xingcheng
 *@version 2015年11月25日
 */
-public class CareerFrame {
+
+@SuppressWarnings("serial")
+public class CareerFrame extends MyFrame{
+	private CareerInitalPanel inital;
+	private CareerJump careerPanel;
+	private Element config;
+	
+	
+
+	public CareerFrame(Element config){
+		super(config);
+		this.config = config;
+		
+	}
 
 }
