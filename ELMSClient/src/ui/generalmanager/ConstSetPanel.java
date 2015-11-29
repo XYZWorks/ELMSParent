5345b55936b6f1ef4ffd39e82c64667d323d15f0
ymcfy <709832926@qq.com>
2015-11-29 16:59:25 +0800

    23

--- ELMSClient/src/ui/generalmanager/ConstSetPanel.java
+++ ELMSClient/src/ui/generalmanager/ConstSetPanel.java
@@ -8,33 +8,81 @@ import ui.tools.MyPanel;
 import ui.tools.MyTextField;
 import ui.util.CompomentType;
 
-/**
- * 指定常量界面
- * @author xingcheng
- *
- */
 @SuppressWarnings("serial")
 public class ConstSetPanel extends MyPanel{
+	
+	private MyLabel transportWay;
+	private MyLabel plane;
+	private MyLabel train;
+	private MyLabel truck;
 
+	private MyLabel instanceInCity;
+	
+	private MyLabel packageCost;
+	private MyLabel paper;
+	private MyLabel wood;
+	private MyLabel plastic;
+	
+	private MyLabel ratios;
+	
+	private MyTextField instance;
+	private MyTextField planeCost;
+	private MyTextField trainCost;
+	private MyTextField truckCost;
+	private MyTextField paperCost;
+	private MyTextField woodCost;
+	private MyTextField plasticCost;
+	
+	private MyButton confirm;
+	private MyButton cancel;
+	
+ 
 	public ConstSetPanel(Element config) {
 		super(config);
+		initLables(config.element(CompomentType.LABLES.name()));
+		initButtons(config.element(CompomentType.BUTTONS.name()));
+		initTextFields(config.element(CompomentType.TEXTFIELDS.name()));
+		addCompoment();
+		addListener();
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected void initButtons(Element e) {
+		confirm = new MyButton(e.element("confirm"));
+		cancel = new MyButton(e.element("cancel"));
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void initTextFields(Element e) {
+		instance = new MyTextField(e.element("instance"));
+		planeCost = new MyTextField(e.element("planeCost"));
+		trainCost = new MyTextField(e.element("trainCost"));
+		truckCost = new MyTextField(e.element("truckCost"));
+		paperCost = new MyTextField(e.element("paperCost"));
+		woodCost = new MyTextField(e.element("woodCost"));
+		plasticCost = new MyTextField(e.element("plasticCost"));		
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	protected void initLables(Element e) {
+		transportWay = new MyLabel(e.element("transportWay"));
+		plane = new MyLabel(e.element("plane"));
+		train = new MyLabel(e.element("train"));
+		truck = new MyLabel(e.element("truck"));
+
+		instanceInCity = new MyLabel(e.element("instanceInCity"));
+		
+		packageCost = new MyLabel(e.element("packageCost"));
+		paper = new MyLabel(e.element("paper"));
+		wood = new MyLabel(e.element("wood"));
+		plastic = new MyLabel(e.element("plastic"));
+		
+		ratios = new MyLabel(e.element("ratios"));
 		// TODO Auto-generated method stub
 		
 	}
@@ -47,8 +95,30 @@ public class ConstSetPanel extends MyPanel{
 
 	@Override
 	protected void addCompoment() {
-		// TODO Auto-generated method stub
+		this.add(transportWay);
+		this.add(plane);
+		this.add(train);
+		this.add(truck);
+
+		this.add(instanceInCity);
 		
+		this.add(packageCost);
+		this.add(paper);
+		this.add(wood);
+		this.add(plastic);
+		
+		this.add(ratios);
+		
+		this.add(instance);
+		this.add( planeCost);
+		this.add( trainCost);
+		this.add( truckCost);
+		this.add( paperCost);
+		this.add(woodCost);
+		this.add(plasticCost);
+		
+		this.add( confirm);
+		this.add( cancel);		
 	}
 
 	@Override
@@ -56,5 +126,5 @@ public class ConstSetPanel extends MyPanel{
 		// TODO Auto-generated method stub
 		
 	}
-	
+
 }
