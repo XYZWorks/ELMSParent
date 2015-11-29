df98dccfb28f38b55e7d3f990a230ba4bb8afe92
ymcfy <709832926@qq.com>
2015-11-29 15:26:10 +0800

    234

---
+++ ELMSClient/src/ui/inital/testPanelforymc.java
@@ -0,0 +1,33 @@
+package ui.inital;
+
+import org.dom4j.Element;
+
+import ui.config.ParseXML;
+import ui.generalmanager.ConstSetPanel;
+import ui.tools.MyFrame;
+
+/** 
+ * @author ymc 
+ * @version 创建时间：2015年11月29日 下午3:09:02 
+ *
+ */
+public class testPanelforymc extends MyFrame{
+
+	public testPanelforymc(Element e) {
+		super(e);
+		
+		InitalPanel initalPanel = new InitalPanel(e.element("initialpanel") , this , null);
+
+		
+
+		initalPanel.add(new ConstSetPanel(e.element("initialpanel").element("ConstSetPanel")));
+		this.setContentPane(initalPanel);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		ParseXML parseXML = new ParseXML("ymc.xml");
+		testPanelforymc test = new testPanelforymc(parseXML.getRoot());
+	}
+
+}
