00f22295f7baebcdc22e79810dc7c421c192829d
czqInNanjing <1300236145@qq.com>
2015-11-29 15:14:36 +0800

    推一发

---
+++ ELMSClient/src/ui/util/MyPictureButtonListener.java
@@ -0,0 +1,37 @@
+package ui.util;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import ui.tools.MyPictureButton;
+
+/**
+ * 按钮监听抽象父类
+ * 
+ * @author czq
+ * @version 2015年11月29日 下午2:50:15
+ */
+public class MyPictureButtonListener extends MouseAdapter {
+	private MyPictureButton button;
+
+	public MyPictureButtonListener(MyPictureButton button) {
+		this.button = button;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		button.setMyIcon(ButtonState.MOUSE_CLICKED);
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		button.setMyIcon(ButtonState.MOUSE_ENTERED);
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		button.setMyIcon(ButtonState.NORMAL);
+	}
+
+}
