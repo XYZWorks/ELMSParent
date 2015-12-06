11d8c4f98bb88e417661b180047df13393bba656
ymcfy <709832926@qq.com>
2015-12-06 14:10:21 +0800

    23

--- ELMSClient/src/ui/tools/MyScrollerPane.java
+++ ELMSClient/src/ui/tools/MyScrollerPane.java
@@ -14,5 +14,9 @@ import ui.table.MyTable;
 @SuppressWarnings("serial")
 public class MyScrollerPane extends JScrollPane {
 	
-	
+	public MyScrollerPane(MyPanel itPanel) {
+		super(itPanel);
+		//设置的初始化
+		itPanel.setPreferredSize(new Dimension(848,752));
+	}
 }
